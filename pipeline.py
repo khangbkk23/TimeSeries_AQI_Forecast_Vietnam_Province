953c820b780bcc399cab7d9628302280ad1665e5
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2025-10-14 23:46:55 +0700

    update pipeline

---
+++ pipeline.py
@@ -0,0 +1,9 @@
+## Import the libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os, shutil
+
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
